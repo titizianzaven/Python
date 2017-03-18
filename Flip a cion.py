c6c4a0f756422eb36c44bff67c5de320818b3b81
titizianzaven <titizian.zaven@yahoo.com>
2017-03-18 01:19:20 -0400

    Add files via upload

---
+++ Flip a cion.py
@@ -0,0 +1,7 @@
+from random import choice
+
+var = 1
+while var == 1 :
+    i = raw_input('Heads or Tails: ')
+    a = ['Heads', 'Tails', 'Oops it full in a crack who says its a 50 50 percent chance.']
+    print choice(a)
